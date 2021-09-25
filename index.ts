674400875b414bf3e90c8253950c0343be1194e9
BR4INL3SS <hyanimehdi115@gmail.com>
2021-09-25 03:53:52 +0100

    Added error handling in cli

--- index.ts
+++ index.ts
@@ -3,15 +3,26 @@ import { argv, exit } from "process";
 import { User } from './types/index'
 import fs from 'fs';
 
+if (argv.length < 4) {
+    console.log("\x1b[31m%s\x1b[0m",'Process failed: Not enough arguments')
+    exit(-1);
+}
 const sampleSize: number = parseInt(argv[2]);
 const result: Array<User> = [];
-if(!sampleSize) exit(-1);
+if(!Number.isInteger(sampleSize)){
+    console.log("\x1b[31m%s\x1b[0m",'Process failed: The sample size should be a number')
+    exit(-1);
+}
 
 console.clear();
 console.log('Progress:', 0, '%')
 for (let index = 0; index < sampleSize; index++) {
     try {
-        const res = execSync(`node ./node_modules/intermock/build/src/cli/index.js --files ./types/index.ts --interfaces "${argv[3]}" -o string`).toString();
+        const res = execSync(`node ./node_modules/intermock/build/src/cli/index.js --files ./types/index.ts -i "${argv[3]}" -o string`).toString();
+        if(res === '{};\n'){
+            console.log("\x1b[31m%s\x1b[0m",`Process failed: Interface ${argv[3]} undefined`)
+            exit(-1);
+        }
         result.push(JSON.parse(res.replace(';', ''))[argv[3]])
         console.clear();
         console.log('Progress:', Math.floor(((index+1)/parseInt(argv[2]))*100), '%')
--- types/index.ts
+++ types/index.ts
@@ -1,21 +1,3 @@
-export interface Student extends User {
-   /** @mockType {random.number(100000)} */
-   id: number,
-   gpa: 1.0 | 2.0 | 3.0 | 4.0,
-}
-
-export interface Professor extends User {
-   /** @mockType {random.number(100000)} */
-   id: number,
-   assignedCourses: Course[],
-}
-
-export interface Course {
-   courseCode: string,
-   /** @mockType {random.number(10)} */
-   section: number,
-}
-
 export interface User {
    firstName: string;
    lastName: string;
@@ -23,6 +5,25 @@ export interface User {
    emailAddress: string;
    classification: "Freshman" | "Sophomore" | "Junior" | "Senior",
    housing: "AUI CAMPUS" | "FARAH INN" | "DOWNTOWN",
+}
+export interface Student{
+   profile: User,
+   /** @mockType {random.number(100000)} */
+   id: number,
+   gpa: 1.0 | 2.0 | 3.0 | 4.0,
+}
+
+export interface Professor{
+   profile: User,
+   /** @mockType {random.number(100000)} */
+   id: number,
+   assignedCourses: Course[],
+}
+
+export interface Course {
+   courseCode: 'SSE XXXX' | 'SHSS XXXX' | 'SBA XXXX',
+   /** @mockType {random.number(10)} */
+   section: number,
 }
 
 export interface Class extends Course{
